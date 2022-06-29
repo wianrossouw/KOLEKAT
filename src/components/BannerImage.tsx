31d5c0a682512e1d7e3dd1e1b8cd9ff54ee94f43
wianrossouw <53365556+wianrossouw@users.noreply.github.com>
2022-06-29 14:49:18 +0200

    Update BannerImage.tsx

--- src/components/BannerImage.tsx
+++ src/components/BannerImage.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import mypic from '../images/BannerImage.jpg'
+import mypic from './bannerimage.jpg'
 const BannerImage = (props) => {
     return (<div style={{ display: 'flex', width: '100%', margin: "0 auto" }}>
         <Image
