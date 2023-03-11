bcc1a5e4a00081f961de930a36325cfea7c0db20
wianrossouw <53365556+wianrossouw@users.noreply.github.com>
2023-03-11 16:17:20 +0200

    Update next-seo.config.ts

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,5 +1,5 @@
 const description =
-  "Launch your own fully automated store with Snipcart, Printful, and Next.js";
+  "High-quality, handmade garments";
 const title = "KOLEKAT®";
 const url = "https://swag.nextdropshippingstarter.com";
 
